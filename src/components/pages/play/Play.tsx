76a1a5cbb11717355721bbfc2f394b7c7110dadf
Jeoff Morris <jeoffmorris@icloud.com>
2022-01-01 06:43:17 +0000

    resized test board to 768px

--- src/components/pages/play/Play.tsx
+++ src/components/pages/play/Play.tsx
@@ -5,7 +5,7 @@ import Shell from "../../../assets/images/shells/white-shell.png";
 
 let data: ISungkaBoard = {
   boardBackgroundImage: "url(" + BoardImage + ")",
-  boardWidth: 300,
+  boardWidth: 768,
   counterImgSrc: "url(" + Shell + ")",
   holesData: [
     {
