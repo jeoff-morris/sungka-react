31b1cf99c5e1d8ff3024ac7ed33922662e8bf96f
Jeoff Morris <jeoffmorris@icloud.com>
2022-01-02 17:45:00 +0000

    changed env to json variables

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDU7svu9g-jM96lZS-OVhFDlrKZW_YSG5c",
+  authDomain: "sungka-app.firebaseapp.com",
+  projectId: "sungka-app",
+  storageBucket: "sungka-app.appspot.com",
+  messagingSenderId: "250834379578",
+  appId: "1:250834379578:web:ec85fc04545d38265a2603",
+  measurementId: "G-Z4ZL5ZQTCQ",
 };
 
 export const app = initializeApp(firebaseConfig);
