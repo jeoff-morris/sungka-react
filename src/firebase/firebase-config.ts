005df7d5ad4a026b855932588c0cdf51059a9ee0
Jeoff Morris <jeoffmorris@icloud.com>
2022-01-02 16:58:27 +0000

    updated .env file

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJiLVXo3dAsJrUm9rtiapPbVwlcU1d75Y",
-  authDomain: "sungka-react.firebaseapp.com",
-  projectId: "sungka-react",
-  storageBucket: "sungka-react.appspot.com",
-  messagingSenderId: "118064236573",
-  appId: "1:118064236573:web:2a8089046320ba86e0def4",
-  measurementId: "G-L77DE69RT3",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.PUBLIC_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
